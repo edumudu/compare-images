53fe4d8bf7d7d1e9bac7a021656db0f1be1323ad
Eduardo Wesley <eduardomudutiu@gmail.com>
2021-06-13 00:20:30 -0300

    fix: fix pick_new_name function

--- src/utils/naming.py
+++ src/utils/naming.py
@@ -2,5 +2,6 @@ from os.path import join, exists
 
 def pick_new_name(dir, filename, count = 1):
   new_path = join(dir, filename)
+  file, _, file_ext = filename.rpartition('.')
 
-  return new_path if exists(new_path) else pick_new_name(dir, f'{filename}-{count}', count + 1)
+  return pick_new_name(dir, f'{file}-{count}.{file_ext}', count + 1) if exists(new_path) else new_path
