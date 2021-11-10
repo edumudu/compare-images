cf0037f5c0b16616aa04d070d46b8a733c92baa6
Eduardo Wesley <eduardomudutiu@gmail.com>
2021-11-09 21:01:27 -0300

    fix: fix pick new name function

--- src/utils/naming.py
+++ src/utils/naming.py
@@ -1,7 +1,13 @@
+import re
+
 from os.path import join, exists
 
-def pick_new_name(dir, filename, count = 1):
+def pick_new_name(dir, filename):
   new_path = join(dir, filename)
-  file, _, file_ext = filename.rpartition('.')
+  get_count_regex = r'_(\d+)(?!.*\d)'
 
-  return pick_new_name(dir, f'{file}-{count}.{file_ext}', count + 1) if exists(new_path) else new_path
+  search_result = re.search(get_count_regex, filename)
+  current_count = int(search_result.group(1)) + 1 if search_result else 1
+  new_filename = re.sub(get_count_regex, f'_{current_count}', filename) if search_result else re.sub(r'(\.+)(?!.*\.)', f'_{current_count}.', filename)
+
+  return pick_new_name(dir, new_filename) if exists(new_path) else new_path
